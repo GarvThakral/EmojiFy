72d57f79f8e71586fa16022d32f29f7be708ae9c
Garv Thakral <garvthakralfx@gmail.com>
2025-08-17 16:06:19 +0530

    EmojifyV2 frontend

--- frontend.py
+++ frontend.py
@@ -6,19 +6,14 @@ import numpy as np
 @st.cache_resource
 def load_model():
 
-    with open("datass/Files/glove.6B.50d.txt", 'r', encoding="utf-8") as f:
-        word_to_vec_map = {}
-        word_to_index_map = {}
-        index_to_word_map = {}
-        index = 1
-        for line in f:
-            values = line.split()
-            word = values[0]
-            vector = np.asarray(values[1:], dtype='float32')
-            word_to_vec_map[word] = vector
-            word_to_index_map[word] = index
-            index_to_word_map[index] = word
-            index+=1
+    with open("test.json" , 'r') as f:
+        lines = f.readlines()
+        print(type(lines[0]))
+        print("Starting ...")
+        print(lines[0][:10])
+        print(lines[0][-10:])
+        print("...ending")
+        word_to_index_map = json.loads(lines[0])
     model = tf.keras.models.load_model('my_model.keras')
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
